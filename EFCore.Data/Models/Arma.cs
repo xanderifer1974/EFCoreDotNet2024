037935b82e5f02b94e1e0913792a4c4e2cb18b6b
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2024-03-28 14:32:46 -0300

    Criando uma estrutura básica de arquitetura no projeto

---
+++ EFCore.Data/Models/Arma.cs
@@ -0,0 +1,10 @@
+﻿namespace EFCore.Data.Models
+{
+    internal class Arma
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public Heroi? Heroi { get; set; }
+        public int HeroiId { get; set; }
+    }
+}
---
+++ EFCore.Data/Models/Heroi.cs
@@ -0,0 +1,11 @@
+﻿namespace EFCore.Data.Models
+{
+    public class Heroi
+    {
+
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public Batalha? Batalha { get; set; }
+        public int BatalhaId { get; set; }
+    }
+}
