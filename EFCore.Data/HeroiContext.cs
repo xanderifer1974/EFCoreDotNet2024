22d12d5991ccbaea604257e387fa518c2f6800b8
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2024-03-31 19:58:02 -0300

    Organizando Referências

--- EFCore.Data/HeroiContext.cs
+++ EFCore.Data/HeroiContext.cs
@@ -6,19 +6,18 @@ namespace EFCore.Repository
 {
     public class HeroiContext: DbContext
     {
+
+        public HeroiContext(DbContextOptions<HeroiContext> options) : base(options)
+        {
+
+        }
+
         public DbSet<Heroi> Herois { get; set; }
         public DbSet<Batalha> Batalhas { get; set; }
         public DbSet<Arma> Armas { get; set; }
         public DbSet<HeroiBatalha> HeroisBatalhas{ get; set; }
         public DbSet<IdentidadeSecreta> IdentidadeSecretas { get; set; }
-
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            var connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Treinamentos\\Udemy\\EFCore\\EFCore\\EFCore.Data\\DataBase\\HeroiDb.mdf;Integrated Security=True";
-
-            optionsBuilder.UseSqlServer(connectionString);
-        }
+          
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
