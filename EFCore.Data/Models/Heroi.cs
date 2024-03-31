4ca92517271ffbfff21be6f251fc180516723680
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2024-03-31 18:07:14 -0300

    Relacionamento Muitos pra Muitos

--- EFCore.Data/Models/Heroi.cs
+++ EFCore.Data/Models/Heroi.cs
@@ -5,7 +5,8 @@
 
         public int Id { get; set; }
         public string Nome { get; set; }
-        public Batalha Batalha { get; set; }
-        public int BatalhaId { get; set; }
+        public List<Arma> Armas { get; set; }
+        public List<HeroiBatalha> HeroisBatalhas { get; set; }
+       
     }
 }
---
+++ EFCore.Data/Models/HeroiBatalha.cs
@@ -0,0 +1,11 @@
+﻿namespace EFCore.Data.Models
+{
+    public class HeroiBatalha
+    {
+        public int HeroiId { get; set; }
+        public Heroi Heroi { get; set; }
+        public int BatalhaId { get; set; }
+        public Batalha Batalha { get; set; }
+
+    }
+}
