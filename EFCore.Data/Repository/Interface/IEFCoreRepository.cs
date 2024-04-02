a2558206648ce35bba7f2b5d3696e86f73810188
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2024-04-02 14:40:43 -0300

    Criando a interface Repository e a classe Repository

---
+++ EFCore.Data/Repository/Interface/IEFCoreRepository.cs
@@ -0,0 +1,20 @@
+﻿using EFCore.Model.Models;
+
+namespace EFCore.Repository.Repository.Interface
+{
+    public interface IEFCoreRepository
+    {
+        void Add<T>(T entity) where T : class;
+        void Update<T>(T entity) where T : class;
+        void Delete<T>(T entity) where T : class;
+
+        Task<bool> SaveChangeAsync();
+
+        Task<Heroi[]> GetAllHerois(bool incluirBatalha = false);
+        Task<Heroi> GetHeroiById(int id, bool incluirBatalha = false);
+        Task<Heroi[]> GetHeroisByNome(string nome, bool incluirBatalha = false);
+
+        Task<Batalha[]> GetAllBatalhas(bool incluirBatalha = false);
+        Task<Batalha> GetBatalhaById(int id, bool incluirBatalha = false);
+    }
+}
